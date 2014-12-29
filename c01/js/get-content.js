81e9aa5e9d4af6cbf17d7d1706bbe12c28f65aaa
andrewdeacy <adeacydevelopment@gmail.com>
2014-12-29 17:04:26 -0600

    First lesson in book finished

---
+++ c01/js/get-content.js
@@ -0,0 +1,16 @@
+var today = new Date(); //Create a new date object
+var hourNow = today.getHours(); //Find the current hour
+var greeting;
+
+//Display the appropriate greeting based on the current time
+if (hourNow > 18){
+	greeting = 'Good Evening!';
+} else if (hourNow > 12){
+	greeting = 'Good Afternoon!';
+} else if (hourNow > 0){
+	greeting = 'Good morning!';
+} else {
+	greeting = 'Welcome';
+}
+
+document.write('<h3>' + greeting + '</h3>');
